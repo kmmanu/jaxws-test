58ee7884d591e1ea6aea0a411b419c1e4b5dff19
manu <manukm07@gmail.com>
2013-05-19 22:44:43 +0530

    Initial create.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ jaxws-test
 ==========
 
 Simple JAX WS  example with Maven
+
+Access the web service on  http://localhost:8080/jaxws-test/HelloWs
---
+++ src/main/java/org/manu/jaxws/HelloWs.java
@@ -0,0 +1,17 @@
+package org.manu.jaxws;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class HelloWs {
+	public String sayHello(String name) {
+		if (name == null) {
+			return "Hello";
+		}
+
+		return "Hello, " + name + "!";
+	}
+}
